5b717d099ff85c74191f3584de674f5090c17a6a
Leon Gritsyuk <lgritsyk@gmail.com>
2023-07-17 21:58:46 +0200

    -Added examples of secret files.

---
+++ constants_example.py
@@ -0,0 +1,12 @@
+URL = "https://www.the-fizz.com/en/student-accommodation/prague/#apartment"
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+}
+READMORE_BUTTON = "//body[1]/main[1]/div[1]/div[7]/div[1]/div[1]/div[2]/div[2]/a[1]"
+ROOM_EURO_XPATH = "//body/main[@id='main']/div[@class='content wrapper']/div[@id='apartment']/div[@class='useable-area']/div[@class='popups']/div[@class='popup popup--apartments prag js-active']/div[@class='popup__inner']/div[@class='flex flexgrid--wide flex--wrap']/div[@class='second-currency']/div[@id='building-form-currency']/div[@class='pex-rooms-in-building']/div[2]/div[2]/div[4]"
+ROOM_CZK_XPATH = "//body/main[@id='main']/div[@class='content wrapper']/div[@id='apartment']/div[@class='useable-area']/div[@class='popups']/div[@class='popup popup--apartments prag js-active']/div[@class='popup__inner']/div[@class='flex flexgrid--wide flex--wrap']/div[@id='building-form']/div[@class='pex-rooms-in-building']/div[2]/div[2]/div[4]"
+CHROMEDRIVER_PATH = "Chromedriver/chromedriver.exe"
+COOKIE_BUTTON_CLASS = "_brlbs-refuse-btn"
+ROOM_CLASS = "room-type-information-no-offer col-xs-12 alert alert-danger not-available"
+TELEGRAM_USERNAME = '#############'
